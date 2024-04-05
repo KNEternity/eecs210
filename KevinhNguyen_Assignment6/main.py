6c5044cec3eb0c260d7be98eb4df4d5d705e58e8
unknown <applekevinh@gmail.com>
2024-04-04 23:06:56 -0500

    PROBLEM 2 DONE

--- KevinhNguyen_Assignment6/main.py
+++ KevinhNguyen_Assignment6/main.py
@@ -30,31 +30,42 @@ def bezoutGCD(integerA, integerB):
     x = max(integerA, integerB)
     y = min(integerA, integerB)
     bezoutLines = []
-    substitutions = {}
+    print("\nForward Steps")
     while y != 0:
         r = x % y 
         divides = x // y 
         bezoutLines.append([x, y, divides, r])
-        if r != integerA and r != integerB and r != 0: 
-            substitutions[r] = "%s - %s * %s" % (x, divides, y)
-
         print("%s = %s * %s + %s" % (x, y, divides, r))
         x = y  # Update x to y
         y = r  # Update y to remainder
-    
-    gcd = x
-        
-    print("\nBackward steps:")
-    s, t = 0, 1
-    for i in range(len(bezoutLines) - 1, 0, -1):
-        a, b, q, _ = bezoutLines[i]
-        a_next, b_next, _, _ = bezoutLines[i - 1]
-        s, t = t, s - q * t
-        print(f"{gcd} = {a_next} - {q} * ({a})")
 
-    
-    print(bezoutLines)
-    print(substitutions)
+    gcd = x 
+    bezoutLines.pop()
+    bezoutLines.reverse()
+
+    print("\nBackward steps:")
+    a,b,c,d = bezoutLines[0]
+    c1 = c
+    c2 = c
+    s, t = 0, 1
+    for i in range(len(bezoutLines)):
+        if i + 1 < len(bezoutLines):
+            a_next, b_next, c_next, d_next = bezoutLines[i + 1]
+            s, t = t, s - c_next * t
+            if i % 2 == 0:
+                print(f"{gcd} = {c1} * {a} - {c2} * {b}")
+                print(f"{gcd} = {c1} * {a} - {c2} * ({a_next} - {c_next} * {a})")
+            else:
+                print(f"{gcd} = {c1} * {a} - {c2} * {b}")
+                print(f"{gcd} = {c1} * ({a_next} - {c_next} * {a}) - {c2} * {b}")
+            c1 = c1 + c_next
+            c2 = c2 + c2
+            a = b_next
+            b = a_next
+            
+            
+    print(f"{gcd} = {c2} * {b} - {c1} * {a}")
+    print()
 
 
 
@@ -66,6 +77,10 @@ def main():
     printGCD(12,42)
     printGCD(252,198)
     print("="*10 + "PROBLEM 2" + "="*10)
+    bezoutGCD(414,662)
+    bezoutGCD(6,14)
+    bezoutGCD(24,36)
+    bezoutGCD(12,42)
     bezoutGCD(252,198)
 
 
