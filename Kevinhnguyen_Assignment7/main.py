599fe25d80978b77cab836e61a3c27a52af8a14c
unknown <applekevinh@gmail.com>
2024-04-18 23:01:35 -0500

    donelab7

--- Kevinhnguyen_Assignment7/main.py
+++ Kevinhnguyen_Assignment7/main.py
@@ -28,7 +28,6 @@ def distguishableDistuigshable(objects, amt, boxes):
         amt -= 1 # Decrement amt
     return result
 
-print(distguishableDistuigshable(52,4,5))
 
 def indistuigshableDistuigshable(objects, boxes):
     """
@@ -38,4 +37,70 @@ def indistuigshableDistuigshable(objects, boxes):
     """
     return combinationsNoRepetition(objects + boxes - 1, objects) 
 
+
+def stirling_second(n, j):
+    """
+    About: Stirling numbers of the second kind
+    Input: n(int of objects), j(int of boxes) 
+    output: combinations(int) 
+    """
+    summation = 0 # Initialize var to represent summation of the terms
+    for i in range(0, j): # Iterate through the range of i from 0 to j
+        summation += combinationsNoRepetition(j,i)*((j-i)**n)*((-1)**i) # Add the term to the summation
+    result = summation//factorial(j) # Divide the summation by j factorial
+    return result
+        
+def distguishableIndistuighable(n, k):
+    """
+    About: calculate distuigshable objects into indistuighable boxes 
+    Input: object n(int), boxes k(int) 
+    Output: cominations(int) 
+    """
+    term = 0 # Initialize var to represent the term
+    for j in range(1, k + 1): # Iterate through the range of j from 1 to k + 1
+        term += stirling_second(n, j) # Add the term to the jth Stirling number of the second kind
+    return term # Return the term
+    
+def pk(n, k): 
+    """
+    About: number of partitions needed for a certain of boxes n
+    Input: objects n(int), boxes k(int)
+    Output: int
+    """
+    if n == 0 and k == 0: # Base case if n and k are both 0
+        return 1
+    if (n <= 0 or k <= 0): # Base case if n or k are less than or equal to 0
+        return 0
+    return pk(n - k, k) + pk(n - 1, k - 1) # Return the sum of the two recursive calls
+
+def indistguishableIndistguishable(n, k):
+    """
+    About: number of combinations of ind,ind objects to boxes
+    Input: object n(int), boxes k(int) 
+    output: combinations(int) 
+    """
+    term = 0 # Initialize var to represent the term
+    for j in range(1, k + 1): # Iterate through the range of j from 1 to k + 1
+        term += pk(n, j) # Add the term to the jth partition number
+    return term
+
+
+#problem 1 
+print("1.")
+print(distguishableDistuigshable(52,4,5))
+print(distguishableDistuigshable(40,4,10))
+
+print()
+print("2.")
 print(indistuigshableDistuigshable(10,8))
+print(indistuigshableDistuigshable(12,6))
+
+print()
+print("3.")
+print(distguishableIndistuighable(4,3))
+print(distguishableIndistuighable(5,4))
+
+print()
+print("4.")
+print(indistguishableIndistguishable(6,4))
+print(indistguishableIndistguishable(5,3))
